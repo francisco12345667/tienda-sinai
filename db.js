b450a42146a72f62051222abe95c721df979a3fe
francisco12345667 <zapatafrank324@gmail.com>
2026-05-31 11:11:30 -0600

    Update db.js

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 import { createClient } from 'https://esm.sh/@supabase/supabase-js';
 
-const supabaseUrl = 'https://tustaujoyzmkfgpfcwoqi.supabase.co'; // Tu URL de proyecto
+const supabaseUrl = 'https://tstaujoyzmkfgpfcwoqi.supabase.co'; // Tu URL de proyecto
 const supabaseAnonKey = 'sb_publishable_CjJm-3O709zXk-8-RX6oVQ_iLtSckaM'; // TU CLAVE QUE ME ACABAS DE PASAR
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
